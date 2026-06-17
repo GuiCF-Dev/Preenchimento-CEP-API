3fa1ab37af972ca0e43b6d15afaa84db476f0d8a
Guilherme Costa Figueiredo <132589851+GuiCF-Dev@users.noreply.github.com>
2026-06-17 16:38:33 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const inputCep = document.querySelector("#cep")
+const botao = document.querySelector("#busca-cep")
+const resultado = document.querySelector("#resultado")
+
+function getValueInput() {
+    const cep = inputCep.value
+
+    getCep(cep)
+}
+
+function renderizaData(data){
+    const {city, address, district, state} = data
+
+    resultado.innerHTML = `
+    <p>Cidade: ${city} - ${state} </p>
+    <p>Bairro: ${district}</p>
+    <p>Rua: ${address}</p>
+    `
+}
+
+async function getCep(cep) {
+    const resposta = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
+    const data = await resposta.json()
+
+    renderizaData(data) 
+}
+
+botao.addEventListener("click", getValueInput)
